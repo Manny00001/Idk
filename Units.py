76a705f9f49e451d9fc1c1992ae10ae558388c76
Manny00001 <144949360+Manny00001@users.noreply.github.com>
2025-08-21 12:36:06 -0400

    Create Units.py

---
+++ Units.py
@@ -0,0 +1,19 @@
+# units.py
+ROSTER = {
+    "Gojo": {
+        "name": "Gojo",
+        "class": "Sorcerer",
+        "level": 5,
+        "hp": 120,
+        "attack": 35,
+        "defense": 25,
+    },
+    "Sukuna": {
+        "name": "Sukuna",
+        "class": "Cursed King",
+        "level": 5,
+        "hp": 140,
+        "attack": 40,
+        "defense": 20,
+    },
+}
